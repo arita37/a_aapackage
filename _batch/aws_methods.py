755258d80a538248f8e0630f3dee3f461641bc51
Gabriel Araujo <gabriel_scf@hotmail.com>
2019-01-30 18:36:28 -0200

    .

--- _batch/aws_methods.py
+++ _batch/aws_methods.py
@@ -9,6 +9,10 @@ import os
 
 from Crypto.PublicKey import RSA
 
+from ansible.executor.playbook_executor import PlaybookExecutor
+from ansible.inventory.manager import Inventory
+from ansible.parsing.dataloader import DataLoader
+
 
 def generatePrivateKey():
     key = RSA.generate(2048)
@@ -23,3 +27,8 @@ def generatePrivateKey():
     pubkey = key.publickey()
     with open("public.key", 'wb') as content_file:
         content_file.write(pubkey.exportKey('OpenSSH'))
+
+
+def setupRemoteInstance(InventoryPath):
+    loader = DataLoader()
+    inventory = Inventory(loader=loader, sources=InventoryPath)
