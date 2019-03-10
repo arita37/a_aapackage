bd78a5a2e146599e39d0d1a408400d5feb37d500
gumnn <arita37@users.noreply.github.com>
2019-03-10 15:07:20 +0900

    Update install.sh

--- zbuild/travis/install.sh
+++ zbuild/travis/install.sh
@@ -3,17 +3,7 @@
 # .travis.yml. See https://docs.travis-ci.com/ for more details.
 # The behavior of the script is controlled by environment variabled defined
 # in the .travis.yml in the top level folder of the project.
-
-# License: 3-clause BSD
-
-# Travis clone scikit-learn/scikit-learn repository in to a local repository.
-# We use a cached directory with three scikit-learn repositories (one for each
-# matrix entry) from which we pull from local Travis repository. This allows
-# us to keep build artefact for gcc + cython, and gain time
-
-
 #  https://conda.github.io/conda-pack/cli.html
-
 
 set -e
 
