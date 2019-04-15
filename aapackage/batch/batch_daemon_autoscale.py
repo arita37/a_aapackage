562211c1519757a95bc207fb17a83411ab5e71df
Ubuntu <ubuntu@ip-172-31-4-122.us-west-2.compute.internal>
2019-04-15 13:44:26 +0000

    ok

--- aapackage/batch/batch_daemon_autoscale.py
+++ aapackage/batch/batch_daemon_autoscale.py
@@ -82,9 +82,115 @@ Out for tasks :
 
 
 '''
-import os,sys
+import os
+import sys
+from time import sleep
+import argparse
+import logging
+import subprocess
 
+################################################################################
+from aapackage import util_log
+from aapackage.batch import util_cpu
 from aapackage import util_aws
+
+
+
+############### logger ########################################################
+global logger
+logger = logging.basicConfig()
+
+
+################################################################################
+def log(*argv):
+  logger.info(",".join([str(x) for x in argv]))
+
+
+def load_arguments():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--task_folder",       default=TASK_FOLDER_DEFAULT, help="path to task folder.")
+  parser.add_argument("--log_file",          default="batchdaemon_autoscale.log", help=".")
+  parser.add_argument("--mode",              default="nodaemon", help="daemon/ .")
+  parser.add_argument("--waitsec", type=int, default=60, help="wait sec")
+  parser.add_argument("--max_instance", type=int, default=2, help="")
+  parser.add_argument("--max_cpu", type=int, default=16, help="")  
+  options = parser.parse_args()
+  return options
+
+
+def start_rule() :
+    pass
+
+
+def stop_rule() :
+    pass
+
+
+def ec2_spot_start(instance_type ):
+    """
+     
+
+    
+    """
+    ss  = 'aws ec2 request-spot-instances   --region us-west-2  --spot-price "0.55" --instance-count 1 '
+    ss += ' --type "one-time" --launch-specification "file://ec2_spot_t3small.json" '
+    
+    msg = os.system(ss )
+
+
+
+def ec2_instance_stop(instance_list) :
+    pass
+
+
+
+def ec2_instance_backup(instance_list, folder_list=["/zlog/"]) :
+    """
+      zip some local folders
+      Tansfer data from local to /zs3drive/backup/AMIname_YYYYMMDDss/
+    
+      Transfer task_out/  to /zs3drive/task_out/
+    
+    """
+    pass
+
+
+
+################################################################################s
+if __name__ == '__main__':
+    args   = load_arguments()
+    logger = util_log.logger_setup(__name__,
+                                   log_file  = args.log_file,
+                                   formatter = util_log.FORMATTER_4,
+                                   isrotate  = True)
+
+
+   instances_dict = {"id" :{ "ncpu":0, "ip_adress": "" }  }
+   
+
+   log("Daemon","start ", os.getpid())
+   while True:
+    log("Daemon new loop", args.task_folder)
+    folders = get_list_valid_task_folder(args.task_folder)
+
+    ntask = len(folders)
+
+    instance_type = start_rule()
+    if instance_type : 
+        ec2_spot_start(instance_type )
+  
+    instance_list = stop_rule(instances_dict)
+    ec2_instance_backup(  instance_list, folder_list=[ "/home/ubuntu/zlog/"])
+    ec2_instance_stop(  instance_list)
+  
+  
+
+    if args.mode != "daemon":
+      log("Daemon","terminated", os.getpid())
+      break
+
+    sleep(args.waitsec)
+
 
 
 
