aa3992bbc69c519176908616cb06d2b21d360401
miloszlakomy <milosz.lakomy@gmail.com>
2019-05-24 08:24:50 +0200

    Import github.com/miloszlakomy/pythoncodeparser

---
+++ pythoncodeparser/src/file_finder.py
@@ -0,0 +1,72 @@
+import os
+
+from src import ast_analyzer
+
+
+def findVariablesInFile(filepath):
+  """Lists variables parsed from the given file
+
+  Args:
+    filepath: A path to a file containing Python code to be parsed
+
+  Returns:
+    A set of 4-tuples describing found variables
+    {(str file,
+      ast.stmt function_or_class,
+      str variable,
+      bool is_local), ...}
+  """
+  ret = set()
+
+  with open(filepath, "r") as source_file:
+    source = source_file.read()
+    variables = ast_analyzer.analyzeSource(source)
+
+    for ((function_or_class, variable_name), is_local) in variables.items():
+      ret.add((filepath, function_or_class, variable_name, is_local))
+
+  return ret
+
+
+def findVariablesInDir(directory):
+  """Lists variables parsed from the given directory
+
+  Args:
+    directory: A path to a directory containing Python code to be parsed
+
+  Returns:
+    A set of 4-tuples describing found variables
+    {(str file,
+      ast.stmt function_or_class,
+      str variable,
+      bool is_local), ...}
+  """
+  ret = set()
+
+  for root, _, files in _walk(
+      directory, onerror=_onerror_reraise, include_hidden=False):
+    for filename in files:
+      if filename.endswith(".py"):
+        filepath = os.path.join(root, filename)
+        ret |= findVariablesInFile(filepath)
+
+  return ret
+
+
+def _onerror_reraise(e):
+  raise e
+
+
+def _walk(*args, include_hidden=None, **kwargs):
+  """A thin wrapper over os.walk which lists only non-hidden files on demand"""
+  if include_hidden is None:
+    raise ValueError("include_hidden must be specified")
+
+  exclude_prefixes = () if include_hidden else (".")
+
+  for root, dirs, files in os.walk(*args, **kwargs):
+    files = [f for f in files if not f.startswith(exclude_prefixes)]
+    dirs[:] = [d for d in dirs if not d.startswith(exclude_prefixes)]
+
+    yield root, dirs, files
+
---
+++ pythoncodeparser/src/output.py
@@ -0,0 +1,28 @@
+import sys
+
+
+def writeCSV(variables, file_handle=sys.stdout):
+  """Writes found variables' data to file_handle in CSV format
+
+  The output is written to standard output by default
+
+  The output starts with a header row:
+  filepath,function_or_class_name,variable_name,is_local
+
+  Function_or_class_name defaults to (global) when there's no enclosing function
+  nor class
+
+  Args:
+    variables: A set of 4-tuples in format provided by the file_finder module
+  """
+  file_handle.write("filepath,function_or_class_name,variable_name,is_local\n")
+
+  for filepath, function_or_class, variable_name, is_local in variables:
+    function_or_class_name = (
+        function_or_class.name
+        if function_or_class is not None
+        else '(global)')
+
+    file_handle.write("%s,%s,%s,%s\n" % (
+        filepath, function_or_class_name, variable_name, is_local))
+
