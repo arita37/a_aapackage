Fixed formula for w

aapackage/control/config.py
@@ -47,7 +47,7 @@ class PricingOptionConfig(Config):
     num_time_interval = 40
     lr_values = list(np.array([5e-3, 5e-3]))
     lr_boundaries = [2000]
-    num_iterations = 100
+    num_iterations = 1000
     num_hiddens = [dim, dim+10, dim+10, dim]
     y_init_range = [1, 10]
 

aapackage/control/main.py
@@ -104,7 +104,7 @@ def main():
             tf.reset_default_graph()
             with tf.Session() as sess:
                 model = FFtf(c, bsde, sess, arg.usemodel)
-                model.build()
+                model.build2()
                 training_history = model.train2()
                 if not os.path.exists('ckpt'):
                     os.makedirs('ckpt')

aapackage/control/solver.py
@@ -261,7 +261,8 @@ class FeedForwardModel(object):
             # y =   tf.reduce_sum( w * (self._x[:, :, t]  / self._x[:, :, t-1]  - 1), 1, keepdims=True)
 
             # all_y.append(y)
-            w = z / tf.reduce_sum(z, -1, keepdims=True)
+            #w = z / tf.reduce_sum(z, -1, keepdims=True)
+            w = z + 1 / tf.sqrt(tf.nn.moments(self._x[:, :, :t], axes=2)[0])
             # w = z
             all_w.append(w)
 